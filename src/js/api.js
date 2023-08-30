b08d267e57eb5637824ccf5495d90abfa9a7a75b
Marii-An <129165317+Marii-An@users.noreply.github.com>
2023-08-30 12:03:04 +0200

    Initial commit

---
+++ src/js/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const URL = 'https://pixabay.com/api/';
+const perPage = 40;
+
+const getImages = async (value, page) => {
+  try {
+    const response = await axios.get(`${URL}`, {
+      params: {
+        key: '39127103-130545e6f30af3924ce3d7b3a',
+        q: `${value}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: page,
+        per_page: perPage,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    Notiflix.Notify.failure('An error occurred while fetching images.');
+    throw error;
+  }
+};
+
+export { getImages };
+
+// Параметр page: Ви передаєте page як параметр функції getImages, але не використовуєте його при виклику axios.get.
---
+++ src/js/index.js
@@ -0,0 +1,112 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getImages } from './api';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  searchButton: document.querySelector('#submit'),
+  searchBox: document.querySelector('.search-box'),
+  gallery: document.querySelector('.gallery'),
+  loadButton: document.querySelector('.load-more'),
+};
+
+let currentPage = 1;
+let query = '';
+
+refs.loadButton.classList.add('is-hidden');
+
+refs.form.addEventListener('submit', searchImages);
+
+async function searchImages(event) {
+  event.preventDefault();
+  refs.gallery.innerHTML = '';
+  currentPage = 1;
+  query = refs.searchBox.value.trim();
+
+  try {
+    const getData = await getImages(query);
+    const { hits, totalHits } = getData;
+
+    if (hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      refs.loadButton.classList.add('is-hidden');
+      return;
+    }
+    Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+
+    refs.gallery.insertAdjacentHTML('beforeend', createGallery(hits));
+    refs.loadButton.classList.remove('is-hidden');
+
+    new SimpleLightbox('.link-lightbox', {
+      captionsData: 'alt',
+      captionDelay: 250,
+      captions: true,
+    }).refresh();
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure('Error fetching images');
+  }
+}
+
+function createGallery(hits) {
+  return hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a href="${largeImageURL}" class="link-lightbox">
+        <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info overlay">
+          <p class="info-item">
+            <b>Likes ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${downloads}</b>
+          </p>
+        </div> 
+      </div>
+      </a>`;
+      }
+    )
+    .join(' ');
+}
+
+refs.loadButton.addEventListener('click', clickOnLoad);
+
+async function clickOnLoad() {
+  currentPage += 1;
+
+  try {
+    const { hits } = await getImages(query, currentPage);
+    refs.gallery.insertAdjacentHTML('beforeend', createGallery(hits));
+    refs.loadButton.classList.remove('is-hidden');
+
+    new SimpleLightbox('.link-lightbox', {
+      captionsData: 'alt',
+      captionDelay: 250,
+      captions: true,
+    }).refresh();
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+}
